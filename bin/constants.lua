00ea10df6199aa9c739daba0fa9e8112d00d90cb
Kyando_2 <amehikoji@gmail.com>
2021-05-05 20:04:48 -0400

    Change size for testing

--- bin/constants.lua
+++ bin/constants.lua
@@ -6,8 +6,8 @@ local consts = {
     IMGP_BASIC = "assets/default.png", -- todo: replace static value with value pulled from XML file.
     IMGP_BACKGROUND =  "assets/background_tile.png",
     IMGP_START_BUTTON = "assets/start_button.png",
-    C_WINDOW_HEIGHT = 800,
-    C_WINDOW_WIDTH = 1000,
+    C_WINDOW_HEIGHT = 400,
+    C_WINDOW_WIDTH = 750,
     -- ..
 }
 
